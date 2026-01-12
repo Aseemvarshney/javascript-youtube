293f77b5b198d7a11063b13777e0a2962b7d43f2
Aseemvarshney <aseemvarshney10@gmail.com>
2026-01-12 13:11:12 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 131212 // cannot change it after if it defined first
+let accountEmail ="aseem@gmail.com"
+var account_password = "1234" //we dont use that much of it 
+account_city = "jaipur"  
+accountId = 3212
+console.log(accountId);
