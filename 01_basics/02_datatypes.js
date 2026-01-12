f4cb3875b48b12c3e317d2b44dacbcaf683abb67
Aseemvarshney <aseemvarshney10@gmail.com>
2026-01-12 13:00:48 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; // means treats all js code as a new version
+
+// alert("hello") // we are using node js not the browser
+
+/*
+integer = number 2 to power 53;
+bigint = bignumber
+boolean = true/false;
+string = ""
+null = standalone value // it is a object
+undefined = 
+symbols = unique
+
+*/
+
+console.log(typeof(null));
