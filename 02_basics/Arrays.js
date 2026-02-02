07316ae80066cba1e66a4a90e023a95e07c5d732
Aseemvarshney <aseemvarshney10@gmail.com>
2026-02-02 07:01:49 +0000

    this the array file in js

--- 02_basics/Arrays.js
+++ 02_basics/Arrays.js
@@ -1,4 +1,29 @@
 // array
 const arr  =[0,1,2,3,4,5]
 
-const heros = ["krish","raone"]
+
+const heros = ["krish","rabon"]
+
+console.log(heros[0]);
+
+// arr.push(6) // add the value at the last 
+// console.log(arr);
+// arr.pop() // delete the last element from the arr
+
+// arr.unshift(9) // add a value at the starting of the array
+// arr.shift() // delete the first index of an array
+// console.log(arr);
+console.log(arr.includes(8));
+const newarr = arr.join()// this will give the output as value not an array
+console.log(newarr);
+
+const myn1 = arr.slice(1,3)
+console.log(myn1) // return the values from 1 t0 3 expect 3
+
+const myn2 = arr.splice(1,3)
+console.log(myn2);// return the value of 1 to 3 with having the 3 rd number 
+
+
+
+
+
